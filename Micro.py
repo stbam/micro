5bc393cf1875006914aea9baace4b3a628a43aa2
stbam <stbamb@gmail.com>
2015-04-26 13:39:03 -0600

    Variables longer than 32 characters are not accepted

--- Micro.py
+++ Micro.py
@@ -1,11 +1,31 @@
-import ply.yacc as yacc
-from MicroYacc import yacc
+#------------------------------------------------------------
+# Micro.py
+# ------------------------------------------------------------
 
-while True:
-    try:
-        s = raw_input('--> ')
-    except EOFError:
-        break
-    if not s:
-    	continue
-    yacc.parse(s)
+import ply.yacc as yacc
+import cmd
+from MicroYacc import yacc
+ 
+class Micro(cmd.Cmd):
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = "--> "
+        self.intro  = "Micro 1.0"
+ 
+    def do_exit(self, args):
+        return -1
+ 
+    def emptyline(self):
+        pass
+ 
+    def default(self, line):
+        try:
+	       	s = yacc.parse(line)
+    		yacc.parse(s)
+    	except:
+    		print('')
+ 
+if __name__ == '__main__':
+	m = Micro()
+	m.cmdloop()
